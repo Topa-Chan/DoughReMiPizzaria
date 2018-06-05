ab8c208ebfaaafc99b5830921b067325cd80ade0
Topanga <tpatterson@student.neumont.edu>
2018-06-05 10:36:11 -0600

    More customize mi

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,72 @@
 //Pizza Canvas
 var pizza = document.getElementById('pizza');
+var i = 0;
 
-//Crusts
-var original = document.getElementById('btn-original');
-var hand = document.getElementById('btn-handToss');
-var thin = document.getElementById('btn-thinCrust');
-//Crust Listeners
+var crustArea = document.getElementById('crust-area');
+var cheeseArea = document.getElementById('cheese-area');
+var sauceArea = document.getElementById('sauce-area');
+var meatArea = document.getElementById('meat-area');
+var veggieArea = document.getElementById('veggie-area');
+var crustBtnNames = [
+                      'Original', 'Hand-Tossed', 'Thin Crust'
+];
+crustBtnNames.name = "Crusts";
+var sauceBtnNames = [
+                      'Marinara', 'Buffalo', 'Creamy Garlic'
+];
+sauceBtnNames.name = "Sauces";
+var cheeseBtnNames = [
+                      'Extra', 'Normal', 'None'
+];
+cheeseBtnNames.name = "Cheeses";
+var meatBtnNames = [
+  'Pepperoni', 'Sausage', 'Beef', 'Ham', 'Chicken'
+];
+meatBtnNames.name = "Meats";
+var veggieBtnNames = [
+  'Mushrooms', 'Pineapple', 'Onions', 'Olives', 'Peppers'
+];
+veggieBtnNames.name = "Veggies";
+var buttons = [];
 
-//Size
-var size = document.getElementById('sizeSelector');
-//Size Listener
+crustBtnNames.forEach(buildButton);
+cheeseBtnNames.forEach(buildButton);
+sauceBtnNames.forEach(buildButton);
+meatBtnNames.forEach(buildButton);
+veggieBtnNames.forEach(buildButton);
 
-//Cheeses
-var extra = document.getElementById('btn-extra');
-var normal = document.getElementById('btn-normal');
-var none = document.getElementById('btn-none');
-//Cheese Listeners
-
-//Sauces
-var marinara = document.getElementById('btn-marinara');
-var creamy = document.getElementById('btn-creamy');
-var buffalo = document.getElementById('btn-buffalo');
-//Sauce Listeners
-
-//Toppings
-//Meats
-var btnpep = document.getElementById('btn-pepperoni');
-var btnsaus = document.getElementById('btn-sausage');
-var btnbeef = document.getElementById('btn-beef');
-var btnham = document.getElementById('btn-ham');
-var btnchick = document.getElementById('btn-chicken');
-//Meat Listeners
-btnpep.addEventListener("click", addPepperoni);
-
-//Veg
-var btnmush = document.getElementById('btn-mushrooms');
-var btnpine = document.getElementById('btn-pineapple');
-var btnogre = document.getElementById('btn-onions');
-var btnolive = document.getElementById('btn-olives');
-var btnpeprs = document.getElementById('btn-peppers');
-//Veg Listeners
-
-var pizzaTops = [];
-
-function addPepperoni(evt) {
-  var pepUrl = "url(../images/preview/SVG/Pepperoni.svg)"
-  pizzaTops.push(pepUrl);
-  var topString = "";
-  for (var i = 0; i < pizzaTops.length; i++) {
-    topString += pizzaTops[i];
-    topString += ", "
+function buildButton(item, index, arr){
+  console.log("Array Name: " + arr.name);
+  console.log("button " + item + " at index " + index + ' created.');
+  buttons[index] = document.createElement('div');
+  buttons[index].textContent = item;
+  // buttons[index].setAttribute('class', 'btn');
+  if (arr.name === "Crusts") {
+    buttons[index].setAttribute('class', 'btn crust');
+    crustArea.appendChild(buttons[index]);
   }
-  console.log(topString);
-  pizza.style.background = topString + "url(../images/preview/SVG/Crust.svg)";
+  if (arr.name === "Cheeses") {
+    buttons[index].setAttribute('class', 'btn cheese');
+    cheeseArea.appendChild(buttons[index]);
+  }
+  if (arr.name === "Sauces") {
+    buttons[index].setAttribute('class', 'btn sauce');
+    sauceArea.appendChild(buttons[index]);
+  } 
+  if (arr.name === "Meats") {
+    buttons[index].setAttribute('class', 'btn meat');
+    meatArea.appendChild(buttons[index]);
+  }
+  if (arr.name === "Veggies") {
+    buttons[index].setAttribute('class', 'btn veggie');
+    veggieArea.appendChild(buttons[index]);
+  }
+  // container.appendChild(buttons[index]);
+  buttons[index].addEventListener('click', btnClicked);
+}
+
+function btnClicked(evt) {
+  console.log(evt.target.innerText + " clicked");
+  evt.target.style.backgroundColor = "#fff";
+  console.log("Parent Class: " + evt.parentNode);
 }
