4f020a061277cbce4e55991c9f3b431bb63e6149
Unknown <jbabylon@student.neumont.edu>
2018-06-05 10:41:24 -0600

    So many things changed

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,69 @@
 //Pizza Canvas
 var pizza = document.getElementById('pizza');
+var i = 0;
 
-//Crusts
-var original = document.getElementById('btn-original');
-var hand = document.getElementById('btn-handToss');
-var thin = document.getElementById('btn-thinCrust');
-//Crust Listeners
+var container = document.getElementById('container');
+var crustBtnNames = [
+                      'Original', 'Hand-Tossed', 'Thin Crust'
+];
+var sauceBtnNames = [
+                      'Marinara', 'Buffalo', 'Creamy Garlic'
+];
+var cheeseBtnNames = [
+                      'Extra', 'Normal', 'None'
+];
+var meatBtnNames = [
+  'Pepperoni', 'Sausage', 'Beef', 'Ham', 'Chicken'
+];
+var veggieBtnNames = [
+  'Mushrooms', 'Pineapple', 'Onions', 'Olives', 'Peppers'
+];
+var buttons = [];
 
-//Size
-var size = document.getElementById('sizeSelector');
-//Size Listener
+crustBtnNames.forEach(buildButton);
+sauceBtnNames.forEach(buildButton);
+cheeseBtnNames.forEach(buildButton);
+meatBtnNames.forEach(buildButton);
+veggieBtnNames.forEach(buildButton);
 
-//Cheeses
-var extra = document.getElementById('btn-extra');
-var normal = document.getElementById('btn-normal');
-var none = document.getElementById('btn-none');
-//Cheese Listeners
-
-//Sauces
-var marinara = document.getElementById('btn-marinara');
-var creamy = document.getElementById('btn-creamy');
-var buffalo = document.getElementById('btn-buffalo');
-//Sauce Listeners
-
-//Toppings
-//Meats
-var btnpep = document.getElementById('btn-pepperoni');
-var btnsaus = document.getElementById('btn-sausage');
-var btnbeef = document.getElementById('btn-beef');
-var btnham = document.getElementById('btn-ham');
-var btnchick = document.getElementById('btn-chicken');
-//Meat Listeners
-btnpep.addEventListener("click", addPepperoni);
-
-//Veg
-var btnmush = document.getElementById('btn-mushrooms');
-var btnpine = document.getElementById('btn-pineapple');
-var btnogre = document.getElementById('btn-onions');
-var btnolive = document.getElementById('btn-olives');
-var btnpeprs = document.getElementById('btn-peppers');
-//Veg Listeners
-
-var pizzaTops = [];
-
-function addPepperoni(evt) {
-  var pepUrl = "url(../images/preview/SVG/Pepperoni.svg)"
-  pizzaTops.push(pepUrl);
-  var topString = "";
-  for (var i = 0; i < pizzaTops.length; i++) {
-    topString += pizzaTops[i];
-    topString += ", "
+function buildButton(item, index, arr){
+  console.log(arr.toString());
+  console.log();
+  console.log("button " + item + " at index " + index + ' created.');
+  buttons[index] = document.createElement('div');
+  buttons[index].textContent = item;
+  console.log("I: " + i);
+  // buttons[index].setAttribute('class', 'btn');
+  if (arr[0] === 'Original') {
+    buttons[index].setAttribute('class', 'btn crust');
   }
-  console.log(topString);
-  pizza.style.background = topString + "url(../images/previews/SVG/Crust.svg)";
+  if (i === 1) {
+    buttons[index].setAttribute('class', 'btn sauce');
+  }
+  if (i === 2) {
+    buttons[index].setAttribute('class', 'btn cheese');
+  } 
+  if (i === 3) {
+    buttons[index].setAttribute('class', 'btn meat');
+  }
+  if (i === 4) {
+    buttons[index].setAttribute('class', 'btn veggie');
+  }
+  container.appendChild(buttons[index]);
+  buttons[index].addEventListener('click', btnClicked);
+}
+
+function createButton(array, item) {
+  var html;
+  for (var index in array) {
+    var btnDiv = document.createElement('div');
+    btnDiv.textContent = item;
+    btnDiv.setAttribute('class', 'btn crust');
+    html += btnDiv;
+  }
+}
+
+function btnClicked(evt) {
+  console.log(evt.target.innerText + " clicked");
+  console.log(evt.srcElement.className);
 }
